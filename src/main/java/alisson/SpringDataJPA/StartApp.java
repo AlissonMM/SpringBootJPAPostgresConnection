5bae4d676a5d059306fedcea5eef8c5a0a8306dd
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-03-11 19:05:36 -0300

    Add database queries in UserRepository, Create insertUser method in StartApp

--- src/main/java/alisson/SpringDataJPA/StartApp.java
+++ src/main/java/alisson/SpringDataJPA/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
@@ -13,19 +15,37 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+//        List<User> users = repository.filtrarPorNome("Test");
+//        List<User> users = repository.findByUserName("testerson");
+        List<User> users = repository.findByNameContaining("Alisson");
+
+
+        users.forEach(System.out::println);
+    }
+
+    private void insertUser(){
         User user = new User();
         user.setName("Alisson");
         user.setUserName("alsso");
         user.setPassword("passwordIsMilk123");
+
+        User user2 = new User();
+        user2.setName("Alisson Mayer Medeji");
+        user2.setUserName("alisson");
+        user2.setPassword("23453454v");
 //        System.out.println(user);
 
+        User user3 = new User();
+        user3.setName("Test");
+        user3.setUserName("testerson");
+        user3.setPassword("0password'_'");
+
         repository.save(user);
+        repository.save(user2);
+        repository.save(user3);
 
-        for (User u: repository.findAll()){
-            System.out.println(u);
-        }
-
-
-
+//        for (User u: repository.findAll()){
+//            System.out.println(u);
+//        }
     }
 }
--- src/main/java/alisson/SpringDataJPA/repository/UserRepository.java
+++ src/main/java/alisson/SpringDataJPA/repository/UserRepository.java
@@ -2,8 +2,21 @@ package alisson.SpringDataJPA.repository;
 
 import alisson.SpringDataJPA.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //JPQL Query Method
+    List<User> findByNameContaining(String name);
+
+    //JPQL Query Method
+    List<User> findByUserName(String userName);
+
+    //Query Override
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name") String name);
 }
